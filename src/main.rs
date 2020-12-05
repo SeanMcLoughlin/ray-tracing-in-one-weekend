661504fe55abe75f707f533d5129af4a6683bb54
Sean McLoughlin <smcloughlin1015@gmail.com>
2020-12-05 13:50:38 -0800

    Section 4

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,56 @@
+mod ray;
 mod vec3;
 
 extern crate pbr;
-use pbr::ProgressBar;
+#[allow(unused_imports)]
+#[macro_use]
 extern crate float_cmp;
-use crate::vec3::Color;
+
+use crate::ray::Ray;
+use crate::vec3::{Color, Point3, Vec3};
+use pbr::ProgressBar;
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Image
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
+    render_image(aspect_ratio, image_width, image_height)
+}
 
-    // Progress bar
-    let mut pb = ProgressBar::new(image_height);
-
+fn render_image(
+    aspect_ratio: f64,
+    image_width: i32,
+    image_height: i32,
+) -> Result<(), Box<dyn Error>> {
     let mut file = File::create("image.ppm")?;
+    let mut pb = ProgressBar::new(image_height as u64);
 
-    // Render
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
     println!("Rendering...");
     writeln!(file, "P3\n{} {}\n255", image_width, image_height)?;
 
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let pixel_color = Color {
-                x: i as f64 / (image_width - 1) as f64,
-                y: j as f64 / (image_height - 1) as f64,
-                z: 0.25,
-            };
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+
+            let ray = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+            let pixel_color = ray_color(&ray);
 
             writeln!(
                 file,
@@ -44,4 +66,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     pb.finish_print("Render finished!");
 
     Ok(())
+}
+
+fn ray_color(ray: &Ray) -> Color {
+    let unit_directions = ray.direction.unit_vector();
+    let t = 0.5 * (unit_directions.y + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
---
+++ src/ray.rs
@@ -0,0 +1,34 @@
+use crate::vec3::{Point3, Vec3};
+
+pub struct Ray {
+    pub origin: Point3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Ray { origin, direction }
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.origin + t * self.direction
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let ray = Ray::new(Point3::new(1.0, 2.0, 3.0), Vec3::new(4.0, 5.0, 6.0));
+        assert_eq!(ray.origin, Point3::new(1.0, 2.0, 3.0));
+        assert_eq!(ray.direction, Vec3::new(4.0, 5.0, 6.0));
+    }
+
+    #[test]
+    fn test_at() {
+        let ray = Ray::new(Point3::new(1.0, 2.0, 3.0), Vec3::new(4.0, 5.0, 6.0));
+        assert_eq!(Point3::new(13.0, 17.0, 21.0), ray.at(3.0));
+    }
+}
