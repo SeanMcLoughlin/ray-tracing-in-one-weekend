cdb44f59ea8c796788d9512c3c9d2aea348fe461
Sean McLoughlin <smcloughlin1015@gmail.com>
2020-12-05 13:55:11 -0800

    Section 5

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,19 @@ fn render_image(
 }
 
 fn ray_color(ray: &Ray) -> Color {
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_directions = ray.direction.unit_vector();
     let t = 0.5 * (unit_directions.y + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> bool {
+    let oc = ray.origin - *center;
+    let a = ray.direction.dot(ray.direction);
+    let b = 2.0 * oc.dot(ray.direction);
+    let c = oc.dot(oc) - radius.powi(2);
+    let discriminant = b.powi(2) - 4.0 * a * c;
+    discriminant > 0.0
 }
